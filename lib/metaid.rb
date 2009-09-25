5a53017b8b542a18c15c139cc655df1a45558d2f
Danny Tatom <dannytatom@gmail.com>
2009-09-24 20:43:25 -0500

    Slight change to README and lib/metaid.rb

--- lib/metaid.rb
+++ lib/metaid.rb
@@ -3,6 +3,7 @@
 # a few simple metaclass helpers
 # 
 # http://github.com/dannytatom/metaid
+
 class Object
   # The hidden singleton lurks behind everyone
   def metaclass; class << self; self; end; end
